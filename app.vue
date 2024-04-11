f9fcd829ab310c8fe6d42a8820d64a7709ad9a4e
Jose Duarte <saert@outlook.com>
2024-04-10 20:17:33 -0400

    Favicon Integration

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+    <Head>
+      <Meta name="description" content="Jose Duarte" />
+      <Link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <Link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <Link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <Link rel="manifest" href="/site.webmanifest" />
+      <Link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <Meta name="msapplication-TileColor" content="#00aba9" />
+      <Meta name="theme-color" content="#ffffff" />
+    </Head>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
--- components/MainHeader.vue
+++ components/MainHeader.vue
@@ -10,39 +10,7 @@
             alt="Float UI logo"
           />
         </a>
-        <button
-          @click="toggleState"
-          class="menu-btn text-gray-500 hover:text-gray-800"
-        >
-          <svg
-            v-if="isOpen"
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          <svg
-            v-else
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-6 h-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-            />
-          </svg>
-        </button>
+        <MenuButton />
       </div>
     </div>
     <Navigation />
@@ -50,8 +18,4 @@
 </template>
 <script setup>
 const isOpen = useState('isOpen')
-
-const toggleState = () => {
-    isOpen.value = !isOpen.value
-}
 </script>
